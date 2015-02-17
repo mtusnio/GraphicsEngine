a61837d4e96ef05ba6e27843e8a436c89194ed39
Michał Tuśnio <m.tusnio@gmail.com>
2015-02-18 00:06:36 +0100

    Basic entity & entitysystem

---
+++ src/entities/Entity.h
@@ -0,0 +1,11 @@
+#ifndef H_ENTITY
+#define H_ENTITY
+
+class Entity
+{
+public:
+	typedef std::string ID;
+};
+
+
+#endif
--- src/entitysystem/IEntitySystem.h
+++ src/entitysystem/IEntitySystem.h
@@ -1,10 +1,19 @@
 #ifndef H_IENTITYSYSTEM
 #define H_IENTITYSYSTEM
 
+#include <string>
+
+#include "../entities/Entity.h"
+
 class IEntitySystem
 {
 public:
+	// Returns entity which identifies itself with this id
+	// nullptr if no entity found
+	virtual Entity * FindEntityByID(Entity::ID id) = 0;
 
+	// Adds an entity to the current entitysystem, removes it from any other systems
+	virtual void AddEntity(Entity * pEnt) = 0;
 };
 
 
