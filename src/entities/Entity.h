ebc3f5ecd5fb26c1e0a40d0242de555257def7dd
Michał Tuśnio <m.tusnio@gmail.com>
2015-02-18 19:27:54 +0100

    Interfaces for entity system

--- src/entities/Entity.h
+++ src/entities/Entity.h
@@ -1,10 +1,33 @@
 #ifndef H_ENTITY
 #define H_ENTITY
 
+#include <string>
+
+class IEntitySystem;
 class Entity
 {
 public:
-	typedef std::string ID;
+	typedef unsigned long long int ID;
+	const ID INVALID_ID = 0;
+
+	Entity();
+	virtual ~Entity();
+
+	// If assigned, will return entity system we are attached to. Otherwise nullptr
+	virtual IEntitySystem * GetEntitySystem() const { return m_EntitySystem;  }
+
+	// Only changes the pointer to this object's entity system, use IEntitySystem
+	// if you want to attach it to an entity system 
+	virtual void SetEntitySystem(IEntitySystem * system) { m_EntitySystem = system; }
+
+	// Changes the object's ID
+	virtual void SetID(Entity::ID id) { m_EntityID = id;  }
+
+private:
+	// Unique ID
+	ID m_EntityID;
+
+	IEntitySystem * m_EntitySystem;
 };
 
 
--- src/entitysystem/IEntitySystem.h
+++ src/entitysystem/IEntitySystem.h
@@ -5,15 +5,26 @@
 
 #include "../entities/Entity.h"
 
+class IScene;
+
 class IEntitySystem
 {
 public:
+	// Returns parent scene of this entity system
+	virtual IScene * GetScene() const = 0;
+
 	// Returns entity which identifies itself with this id
 	// nullptr if no entity found
-	virtual Entity * FindEntityByID(Entity::ID id) = 0;
+	virtual Entity * FindEntityByID(Entity::ID id) const = 0;
 
-	// Adds an entity to the current entitysystem, removes it from any other systems
-	virtual void AddEntity(Entity * pEnt) = 0;
+	// Adds an entity to the current entitysystem, removes it from any other systems.
+	// Returns the object's assigned ID if it exists or if it's been added
+	virtual Entity::ID AddEntity(Entity * pEnt) = 0;
+
+	// Removes an entity from this system, if shouldDelete is set to false
+	// it will not remove the object from memory, thus allowing it
+	// to be attached to a different entity system
+	virtual bool RemoveEntity(Entity * pEnt, bool shouldDelete = true) = 0;
 };
 
 
