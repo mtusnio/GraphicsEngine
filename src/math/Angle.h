2bf79406a76a51ea435666bdf4bb829f5c45e3e9
Michał Tuśnio <m.tusnio@gmail.com>
2015-02-20 04:47:09 +0100

    Proper angle constructors

--- src/math/Angle.h
+++ src/math/Angle.h
@@ -7,7 +7,33 @@ struct Angle
 	float y;
 	float z;
 
+	Angle()
+	{
+		x = 0.0f;
+		y = 0.0f;
+		z = 0.0f;
+	}
 
+	Angle(float x, float y, float z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	Angle(const Angle & rhs)
+	{
+		x = rhs.x;
+		y = rhs.y;
+		z = rhs.z;
+	}
+
+	Angle(const Angle && rhs) : x(std::move(rhs.x)),
+		y(std::move(rhs.y)),
+		z(std::move(rhs.z))
+	{
+
+	}
 };
 
 #endif
