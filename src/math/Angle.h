bfa761ee06110bb4f6e60ed30f082a0304aa1bf9
Michał Tuśnio <m.tusnio@gmail.com>
2015-02-21 03:58:55 +0100

    Constants added, specifically PI for trigonometric functions, and a function for Angle to return Vector

--- src/math/Angle.h
+++ src/math/Angle.h
@@ -1,6 +1,9 @@
 #ifndef H_ANGLE
 #define H_ANGLE
 
+#include <math.h>
+#include "consts.h"
+
 struct Angle
 {
 	float x;
@@ -38,6 +41,17 @@ struct Angle
 	Angle operator-() const
 	{
 		return Angle(-x, -y, -z);
+	}
+
+	Vector ToDirection() const
+	{
+		const float DEG2RAD = (float)Math::PI/180.f;
+
+		float xRad = x * DEG2RAD;
+		float yRad = y * DEG2RAD;
+		float zRad = z * DEG2RAD;
+
+		return Vector(cos(zRad)*cos(yRad), sin(zRad)*cos(yRad), sin(yRad));
 	}
 };
 
---
+++ src/math/consts.h
@@ -0,0 +1,9 @@
+#ifndef H_CONSTS
+#define H_CONSTS
+
+namespace Math
+{
+	const double PI = 3.141592653589793238462643383279502884L;
+}
+
+#endif
