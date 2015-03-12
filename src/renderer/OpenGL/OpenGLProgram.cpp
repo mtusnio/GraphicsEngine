9a30e46a307d5e9dcafcbd2814613e3b6130f8bc
Michał Tuśnio <m.tusnio@gmail.com>
2015-03-12 20:49:14 +0100

    Added OpenGL program encapsulation

---
+++ src/renderer/OpenGL/OpenGLProgram.cpp
@@ -0,0 +1,36 @@
+#include "OpenGLProgram.h"
+
+OpenGLProgram::OpenGLProgram()
+{
+	m_ProgramID = 0;
+}
+
+OpenGLProgram::~OpenGLProgram()
+{
+	FreeProgram();
+}
+
+void OpenGLProgram::Load(const std::shared_ptr<const OpenGLShader> & vertexShader, const std::shared_ptr<const OpenGLShader> & pixelShader)
+{
+	_ASSERT(vertexShader.get() != nullptr && pixelShader.get() != nullptr);
+
+	if (m_ProgramID != 0)
+	{
+		FreeProgram();
+	}
+
+	m_VertexShader = vertexShader;
+	m_FragmentShader = pixelShader;
+
+
+	m_ProgramID = glCreateProgram();
+	glAttachShader(m_ProgramID, m_VertexShader->ShaderID);
+	glAttachShader(m_ProgramID, m_FragmentShader->ShaderID);
+	glLinkProgram(m_ProgramID);
+}
+
+void OpenGLProgram::FreeProgram()
+{
+	glDeleteProgram(m_ProgramID);
+	m_ProgramID = 0;
+}
